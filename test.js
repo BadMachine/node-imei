b2253726ec92221629646071eccb78fa4ba6de39
ME <50546204+BadMachine@users.noreply.github.com>
2019-05-12 17:55:45 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var imei = require ('./scr/imei');
 
-var IMEI= new imei;
+var IMEI= new imei();
 console.log(IMEI.isValid("860921035123120")); // returns true
 
 console.log(IMEI.device("Apple","iPhone3G")); // returns string with imei by device TAC
