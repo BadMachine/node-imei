542e672814b9ee233827daf6eb3ea4723a733a1e
BreakIT <breakthejails@gmail.com>
2023-09-14 14:09:54 +0000

    test refactor

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
 var imei = require ('./scr/imei');
 
-var IMEI= new imei();
-console.log(IMEI.isValid("860921035123120")); // returns true
+console.log(imei.isValid("860921035123120")); // returns true
 
-console.log(IMEI.device("Apple","iPhone3G")); // returns string with imei by device TAC
+console.log(imei.device("Apple","iPhone3G")); // returns string with imei by device TAC
 
-console.log(IMEI.random()); // returns string with random imei
+console.log(imei.random()); // returns string with random imei
